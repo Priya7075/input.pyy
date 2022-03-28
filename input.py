a7c4a24a42897d3b18bc696032cc74ceb50335b6
Priya7075 <102401793+Priya7075@users.noreply.github.com>
2022-03-28 11:01:04 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,16 @@
+# a=input()
+# print(a,type(a))#kavya str
+# print("the value of a is"+a) #the value of a iskavya
+
+# b=input()
+# print(b,type(b)) #indu
+# print("the value of b is "+b) #the value of b is indu
+# c=int(input('enter your phone number:'))
+# print(c,type(c))#1234567 int
+# print("the value of c is",c) #the value of c is 1234567
+# d=float(input('enter your height:'))#6
+# print(d,type(d)) #6.0 float
+# print("the value of d is", d) # the value of d is 6.0
+# e=str(input('enter your name:')) #1234
+# print(e,type(e)) # 1234 str
+# print("yuor name is:", e) #your name is: 1234
